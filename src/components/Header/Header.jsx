43d607c8030666f84a161ff03060e1b0d77bf0e9
Julia Pereira <juliapt95@gmail.com>
2022-11-21 23:19:49 -0300

    fix: bug on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,10 @@ import "./style.css";
 
 export const Header = () => {
   return (
-    <header className="container">
-      <h1 className="title1">Meus personagens</h1>
+    <header>
+      <div className="container">
+        <h1 className="title1">Meus personagens</h1>
+      </div>
     </header>
   );
 };
